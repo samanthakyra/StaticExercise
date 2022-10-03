ef3950d401e1642b6f8b6546f686a1653259a4e4
samanthakyra <samanthakyramendoza@gmail.com>
2022-10-03 11:09:51 -0700

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20); 
+
+            Console.WriteLine("After using the conversion");
+            Console.WriteLine($"The celcius is {celsius}");
+            Console.WriteLine($"The fahrenheit is {fahrenheit}");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (fahrenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double farenheit = (celsius * 9) / 5 + 32;
+
+            return farenheit;
+        }
+    }
+}
